c22d93055576e6717df0dce377eb6b405401dbeb
gavenlee <2274259338@qq.com>
2023-04-01 14:20:55 +0800

    Site updated: 2023-04-01 14:20:54

--- js/persioninfo/skill.js
+++ js/persioninfo/skill.js
@@ -4,3 +4,16 @@ $(function() {   //方法一
          $(this).css("background-color", random);
     });
 });
+
+$(function() {   //方法一
+    if (document.getElementById("category-bar-items")){
+        let xscroll = document.getElementById("category-bar-items");
+        xscroll.addEventListener("mousewheel", function (e) {
+        //计算鼠标滚轮滚动的距离
+        let v = -e.wheelDelta / 2;
+        xscroll.scrollLeft += v;
+        //阻止浏览器默认方法
+        e.preventDefault();
+      }, false);
+    }
+});
